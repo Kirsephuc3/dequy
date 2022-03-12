9a2631f0028f337c44577d2b4eeb7e66e095b2ea
Kirsephuc3 <101470532+Kirsephuc3@users.noreply.github.com>
2022-03-12 23:55:16 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//int giaithua(int n) {
+//	if (n == 1)
+//		return 1;
+//	else
+//		return n * giaithua(n - 1);
+//}
+//int main() {
+//	int n;
+//	do {
+//		cout << " Nhap so n can tinh: ";
+//		cin >> n;
+//		if (n < 0) {
+//			cout << " Nhap sai nhap lai !!!" << endl;
+//		}
+//	} while (n < 0);
+//	cout << " Tong giai thua can tinh la: " << giaithua(n) << endl;
+//	system("pause");
+//	return 0;
+//}
