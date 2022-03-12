c66e8e6288b9e1a615985cd165a31f532c861bfc
Kirsephuc3 <101470532+Kirsephuc3@users.noreply.github.com>
2022-03-12 23:55:29 +0700

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//int Fibonacci(int n)
+//{
+//    if (n == 1 || n == 2)
+//        return 1;
+//    return Fibonacci(n - 1) + Fibonacci(n - 2);
+//}
+//int main() {
+//    int n;
+//    cout << " Nhap n can tinh: ";
+//    cin >> n;
+//    cout << "So Fibonacci thu " << n << " la: " << Fibonacci(n) << endl;
+//    system("pause");
+//    return 0;
+//}
