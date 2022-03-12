871b8bee6079a0ece5b248dc42684e85eb2fa5f7
Kirsephuc3 <101470532+Kirsephuc3@users.noreply.github.com>
2022-03-12 23:55:44 +0700

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//int mu(int x, int n) {
+//	if (n == 1)
+//		return x;
+//	else
+//		return mu(x, n-1)*x;
+//}
+//int main() {
+//	int x, n;
+//	cout << " Nhap so can tinh: ";
+//	cin >> x;
+//	cout << " Nhap so mu can tinh: ";
+//	cin >> n;
+//	cout << " So mu can tinh la: " << mu(x, n) << endl;
+//	system("pause");
+//	return 0;
+//}
